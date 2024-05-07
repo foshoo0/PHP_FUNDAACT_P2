e6554bef898576a6e9eb13e1dc19c32c377efa37
foshoo0 <167631179+foshoo0@users.noreply.github.com>
2024-05-07 22:00:20 +0800

    Add files via upload

---
+++ OMS.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Discriminant Calculator</title>
+</head>
+<body>
+    <form action="compu.php" method="GET">
+        <h1>DIWATA PARES</h1>
+        <ul>
+          <li>Kanin - 10php</li>
+          <li>Regular Pares - 70php</li>
+          <li>Combo Pares - 80php</li>
+        </ul>
+        <label for="order" required>Choose your order:</label>
+        <select name="order" id="order">
+        <option value="kanin" >Kanin</option>
+        <option value="Regular_Pares" >Regular Pares</option>
+        <option value="Combo_Pares"  >Combo Pares</option>
+        </select>
+        <br><br>
+        <label for="orders">Quatity</label>
+        <input type="text" name="hm" required>
+        <br><br>
+        <label for="money">Cash</label>
+        <input type="text" name="cash" required>
+        <br><br>
+        <input type="submit" value="Submit" name="calc">
+      </form>
+</body>
+</html>
---
+++ compu.php
@@ -0,0 +1,32 @@
+<?php  
+if(isset($_GET['calc'])) {
+	$rice = 10;
+	$rpares = 70;
+	$cpares = 100;
+  	$order = $_GET['hm'];
+	$money = $_GET['cash'];
+
+	switch ($_GET['order']) {
+		case 'kanin':
+			$item_price = $rice;
+			break;
+		case 'Regular_Pares':
+			$item_price = $rpares;
+			break;
+		case 'Combo_Pares':
+			$item_price = $cpares;
+			break;
+		default:
+			break;
+	}
+	// Create a variable to store the sum.
+	$total_price = $item_price * $order;
+
+	// Calculate change.
+	$change = $money - $total_price;
+
+	// Print the result
+	echo "Total : " . $total_price . "php";
+	echo "Change : " . $change . "php";
+}
+?>
